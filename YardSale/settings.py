123a958c1202f417488b7c923404da973a302c74
angel <perdonholaperdon123@gmail.com>
2023-04-24 04:18:49 -0400

    Adding staticFiles

--- YardSale/settings.py
+++ YardSale/settings.py
@@ -150,7 +150,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 import os
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR/'YardSale'+'/', 'staticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR/'YardSale', 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
