6ae8588d69f2f570be4cce236747ec330d4e24ce
angel <perdonholaperdon123@gmail.com>
2023-04-24 05:44:34 -0400

    Changing config

--- YardSale/settings.py
+++ YardSale/settings.py
@@ -68,7 +68,6 @@ SOCIALACCOUNT_PROVIDERS = {
 SECURE_SSL_REDIRECT = False
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -153,7 +152,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 import os
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
