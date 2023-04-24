1f6a8feec28c266188b70a9bace5a7a44361bbb6
angel <perdonholaperdon123@gmail.com>
2023-04-24 04:29:20 -0400

    Adding static_dirs

--- YardSale/settings.py
+++ YardSale/settings.py
@@ -151,6 +151,9 @@ USE_TZ = True
 import os
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR/'YardSale', 'staticfiles')
+STATIC_DIRS = [
+    os.path.join(BASE_DIR/'YardSale', 'staticfiles')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
